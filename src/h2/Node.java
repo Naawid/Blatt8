49a60aea610d9516bfaad65437291885be3d9002
Nawid Adel <nawid@MacBook-Air-von-Nawid.fritz.box>
2025-12-12 17:33:17 +0100

    h2

---
+++ src/h2/Node.java
@@ -0,0 +1,11 @@
+package h2;
+
+public class Node {
+	public int value;
+	public Node next;
+
+	public Node(int value) {
+		this.value = value;
+		next = null;
+	}
+}
---
+++ src/h2/SimpleList.java
@@ -0,0 +1,68 @@
+package h2;
+
+public class SimpleList {
+	public Node head;
+
+	public SimpleList() {
+		head = new Node(Integer.MIN_VALUE);
+	}
+
+	public Node getFirst() {
+		return head.next;
+	}
+
+	public Node getLast() {
+		Node current = head.next;
+		if (current == null) return null;
+		else {
+			while (current.next != null)
+				current = current.next;
+		}
+		return current;
+	}
+
+	public void append(int newValue) {
+		Node newNode = new Node(newValue);
+
+		Node last = getLast();
+		if (last == null) {
+			head.next = newNode;
+		} else {
+			last.next = newNode;
+		}
+	}
+
+	public Node findFirst(int value) {
+		Node current = head.next;
+
+		while (current != null) {
+
+			if (value == current.value) return current;
+			current = current.next;
+		}
+		return null;
+	}
+
+	public boolean insertAfter(int preValue, int newValue) {
+		Node target = findFirst(preValue);
+		if (target==null) return false;
+		Node newNode = new Node(newValue);
+		newNode.next = target.next;
+		target.next = newNode;
+		return true;
+	}
+
+	public boolean delete(int value) {
+		Node current = head.next;
+		Node prev = head;
+	while (current != null) {
+		if (current.value == value) {
+			prev.next = current.next;
+		}
+		prev = current;
+		current = current.next;
+	}
+	return false;
+	
+	}
+}
